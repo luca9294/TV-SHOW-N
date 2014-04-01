b6842d3da25ca17ab3e39c91ffa5e682886eadd4
Sarah <Sarah@Sarah-PC>
2014-04-01 13:56:52 +0200

    changes

---
+++ TV_SHOWS/src/info/androidhive/slidingmenu/Episode.java
@@ -0,0 +1,26 @@
+package info.androidhive.slidingmenu;
+
+import org.json.*;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+public class Episode {
+	
+
+	public String id, title, first_aired, overview, image, percentage;
+	
+	public Episode(String id, String title, String first_aired, String overview, String image, String percentage){
+		this.id = id;
+		this.title = title;
+		this.first_aired = first_aired;
+		this.overview = overview;
+		this.image = image;
+		this.percentage = percentage;
+	}
+	
+	
+
+	
+}
